a31b167d30aba1a5966c33d897fa20b146183aca
Anne Reinarz <reinarz@in.tum.de>
2021-04-05 19:48:11 +0200

    remove unneeded test problems, move lock

--- SWE_asagi_limited/InitialData.h
+++ SWE_asagi_limited/InitialData.h
@@ -24,22 +24,9 @@ class InitialData {
   AsagiReader* asagiReader;
 #endif
 
-  void ShockShockProblem(const double* const x,double* Q);
-  void RareRareProblem(const double* const x,double* Q);
-  void GaussFunctionProblem(const double* const x,double* Q);
-  void ExpBreakProblem(const double* const x,double* Q);
-  void DamBreakProblem(const double* const x,double* Q);
-  void SeaAtRestProblem(const double* const x,double* Q);
-  void SteadyRunUpLinear(const double* const x,double* Q);
-  void RunUpLinear(const double* const x,double* Q);
-  void SteadyRunUpShelf(const double* const x,double* Q);
-  void RunUpShelf(const double* const x,double* Q);
-  void WettingDryingProblem(const double* const x, double* Q);
-  void RunUpTest(const double* const x, double* Q);
-  void OscillatingLake(const double* const x, double* Q);
-  void SolitaryWaveOnSimpleBeach(const double*const x, double* Q);
   void readAsagiData(const double* const x,double* Q);
   void readAsagiData_nobath(const double* const x,double* Q);
+  void readAsagiData_onlybath(const double* const x,double* Q);
 };
 
 #endif // __InitialData_CLASS_HEADER__
